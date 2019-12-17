c58bb720c180df69dbf8aa15a4fc8905e72e2580
Nobuhiro MIKI <nob@bobuhiro11.net>
2019-12-17 23:26:05 +0900

    server: add simple websocket handler

--- server/main.go
+++ server/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"golang.org/x/net/websocket"
+	"io"
+	"net/http"
 )
 
+func echo(ws *websocket.Conn) {
+	io.Copy(ws, ws)
+}
+
 func main() {
-	fmt.Println("The catan server started")
+	http.Handle("/echo", websocket.Handler(echo))
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
 }
